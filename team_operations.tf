d1a4558666ae5d490739528a9a05418a60f1efd6
Tyler Heucke <tyler.heucke@dell.com>
2018-05-15 15:22:41 -0700

    Add initial teams and team repositories
    Signed-off-by: Tyler Heucke <tyler.heucke@dell.com>

---
+++ team_operations.tf
@@ -0,0 +1,21 @@
+resource "github_team" "operations" {
+  name        = "Operations"
+  description = "Build/Packaging/Release"
+  privacy     = "closed"
+}
+
+variable "operations_repos" {
+  default = [
+    "continuous-integration",
+    "rootfs",
+    "tools_opx-py",
+  ]
+  type    = "list"
+}
+
+resource "github_team_repository" "operations" {
+  count      = "${length(var.operations_repos)}"
+  permission = "push"
+  repository = "${element(var.operations_repos, count.index)}"
+  team_id    = "${github_team.operations.id}"
+}
